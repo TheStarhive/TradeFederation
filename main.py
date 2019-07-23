e53487409b4198f810c887e760976571f29e2bbe
Starhive <52062394+TheStarhive@users.noreply.github.com>
2019-07-22 21:15:50 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print("Logged in.")
+
+@bot.event
+async def on_member_join(member):
+    for channel in member.guild.channels:
+        if channel.name == "general":
+            await channel.send("Welcome " + member.mention + " to the bot time server!")
+
+@bot.event
+async def on_member_remove(member):
+    for channel in member.guild.channels:
+        if channel.name == "general":
+            await channel.send("Sadly, " + member.name + " just left the server.")
+
+@bot.command()
+async def ping(ctx, *, argument: str):
+    await ctx.send("Pong" + argument)
+
+bot.run(NjAyOTgzMTA1NjQ0NDYyMTEx.XTaIcw.60IFdQeom_lAKVpuITvEPpoi6kM)
